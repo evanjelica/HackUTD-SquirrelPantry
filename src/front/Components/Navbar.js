19eeaf6d9fcee405776b3cc6680b5307593b2271
Anjelica Avorque <anjelicaavorque@MacBook-Pro.local>
2021-02-27 13:56:48 -0600

    Added About and Navbar components

---
+++ src/front/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+
+function Navbar(){
+    return(
+        <div>
+            <Nav className="justify-content-center" activeKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Contact</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </div>
+    );
+}
+export default Navbar;
